4a9037a181dedcf4f2b6e2e6a6908dae38274c9b
Nick Hammond <n.hammond@waracle.com>
2021-06-24 16:35:31 +0100

    fix: Output correct URL for domain name

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "rest_api_id" {
 }
 
 output "api_url" {
-  value = aws_api_gateway_deployment._.invoke_url
+  value = aws_api_gateway_domain_name._[0].regional_domain_name
 }
 
 output "api_domain_name" {
